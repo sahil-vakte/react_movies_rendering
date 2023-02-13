587eb48f6b198c65477762e9b489f302c81e35c1
Sahil <sahilvaktepatil123@gmail.com>
2023-02-14 01:50:35 +0530

    Responce

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -10,13 +10,13 @@ const HomePage = () => {
   useEffect(() => {
     axios
       .post("https://hoblist.com/api/movieList", {
-        category: "Movies",
+        category: "movies",
         language: "kannada",
         genre: "all",
         sort: "voting",
       })
       .then((response) => {
-        setmovies(response.data["queryParam"]);
+        setmovies(response.data["result"]);
       });
   }, [2]);
 
@@ -26,14 +26,17 @@ const HomePage = () => {
       <div className="movies-page-width-div">
       <Navbar/>
         <div className="movies-page-main-div">
+
           <div className="movies-details-page-card-main">
-            <div className="movies-details-page-card">
+        {movies&&movies.map((index)=>(
+          <div style={{borderBottom:"1px solid gray", paddingTop:"10px", paddingBottom:"10px"}}>
+            <div className="movies-details-page-card" style={{paddingTop:"20p"}}>
               <div className="movies-details-page-card-sub-div-one">
                 <div className="movies-details-sub-one-div-two">
                   <IoMdArrowDropup className="vote-arrow-tag" />
                 </div>
                 <div className="movies-details-sub-one-div-two">
-                  <p>1</p>
+                  <p>{index.voting}</p>
                 </div>
                 <div className="movies-details-sub-one-div-two">
                   <IoMdArrowDropdown className="vote-arrow-tag" />
@@ -44,53 +47,61 @@ const HomePage = () => {
               </div>
               <div className="movies-details-page-card-sub-div-two">
                 <img
-                  src="https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z587327/portrait/1920x77079aee11cc5b74ff5a6553e2044f78bf0.jpg"
+                  // src="https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z587327/portrait/1920x77079aee11cc5b74ff5a6553e2044f78bf0.jpg"
+                  src={index.poster}
                   className="movies-img-tag"
                 />
               </div>
 
               <div className="movies-details-page-card-sub-div-three">
-                <div className="movies-details-div-three-sub-one">
-                  <div className="movies-details-title-div-one">
-                    <p>Category</p>
-                  </div>
-                  <div className="movies-details-title-div-two">
-                    <p>{movies.category}</p>
-                  </div>
-                </div>
 
-                <div className="movies-details-div-three-sub-one">
-                  <div className="movies-details-title-div-one">
-                    <p>Language</p>
-                  </div>
-                  <div className="movies-details-title-div-two">
-                    <p>{movies.language}</p>
-                  </div>
-                </div>
+              <div>
+          <h3>{index.title}</h3>
+              </div>
 
-                <div className="movies-details-div-three-sub-one">
+
+
+              <div className="movies-details-div-three-sub-one">
                   <div className="movies-details-title-div-one">
                     <p>Genre</p>
                   </div>
                   <div className="movies-details-title-div-two">
-                    <p>{movies.genre}</p>
+                    <p>{index.genre}</p>
                   </div>
                 </div>
 
                 <div className="movies-details-div-three-sub-one">
                   <div className="movies-details-title-div-one">
-                    <p>Voting</p>
+                    <p>Director</p>
                   </div>
                   <div className="movies-details-title-div-two">
-                    <p>{movies.sort}</p>
+                    <p>{index.director}</p>
                   </div>
+                </div>
+
+                <div className="movies-details-div-three-sub-one">
+                  <div className="movies-details-title-div-one">
+                    <p>Starring</p>
+                  </div>
+                  <div className="movies-details-title-div-two">
+                    <p>{index.stars}</p>
+                  </div>
+                </div>
+
+            
+
+                <div className="movies-details-div-three-sub-one">
+                  <p style={{color:"#00a5ff"}}>{index.pageViews} views | Voted by {index.totalVoted} people</p>
                 </div>
               </div>
             </div>
             <div className="movies-details-page-card-two">
               <button className="watch-trailer-btn">Watch Trailer</button>
             </div>
+            </div>
+        ))}
           </div>
+
         </div>
       </div>
     </div>
